76c50254e5a122a5c2cf4fafba67051e3a6e7521
Abhigyan Singh <abhigyan2311@gmail.com>
2017-01-17 20:13:45 +0530

    update

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ var PubNub = require('pubnub')
 var pubnub = new PubNub({
     subscribeKey: "sub-c-383332aa-dcc0-11e6-b6b1-02ee2ddab7fe",
     publishKey: "pub-c-73cca4b9-e219-4f94-90fc-02dd8f018045",
+    secretKey: "sec-c-YzcyZjI4NzEtNmUxMi00ZDc0LWI4ZGMtNGUyYmFlZmI1OTQ3",
     ssl: false
 })
+
+console.log("Listening...");
 
 pubnub.addListener({
     message: function(m) {
