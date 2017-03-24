5d8f6ea5f90cd7ba3981936a6168f4274e1db981
Abhigyan Singh <abhigyan2311@gmail.com>
2017-03-24 19:42:44 +0530

    Pir Update

--- pi_modules/pir.py
+++ pi_modules/pir.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import time
 from picamera import PiCamera
 from Naked.toolshed.shell import execute_js
+import os.path
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
@@ -26,9 +27,14 @@ while True:
              camera.resolution = (200, 200)
              camera.start_preview()
              time.sleep(1)
-             camera.capture('../piCam/capturedImg.jpg')
+             camera.capture('piCam/capturedImg.jpg')
              camera.stop_preview()
              camera.close()
              count=0
-             time.sleep(5)
-             success = execute_js('../imgTransfer.js')
+             check=0
+             while(check=0):
+                if(os.path.isfile('piCam/capturedImg.jpg')):
+                    success = execute_js('imgTransfer.js')
+                    check=1
+                else:
+                    check=0
