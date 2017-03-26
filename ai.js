aa49824d1b179cacf5405c8fae137557d4b8a796
Abhigyan Singh <abhigyan2311@gmail.com>
2017-03-26 14:07:03 +0530

    Ai Update

--- ai.js
+++ ai.js
@@ -1,5 +1,7 @@
 var apiai = require('apiai');
 var fs = require('fs');
+const gTTS = require('gtts');
+var mpg321 = require('mpg321');
 
 var app = apiai("3e88fac3a4f14428b4ad2eea79eda44e");
 
@@ -7,10 +9,24 @@ var request = app.textRequest('What is the temperature', {
     sessionId: '433c0e9f-bf0a-4fad-8a26-de291a92cc9d'
 });
 
+function playAudio() {
+    var proc = mpg321().file('outputAudio/say.mp3').exec();
+    process.on('SIGINT', function(data) {
+        process.exit();
+    });
+}
+
 request.on('response', function(response) {
     if (response['result']['metadata']['intentName'] == 'temperature') {
-        var file = fs.readFileSync('pi_modules/temperature.txt', "utf8");
-        console.log(file);
+        var temperature = fs.readFileSync('pi_modules/temperature.txt', "utf8");
+        var tempJson = JSON.parse(temperature);
+        console.log(tempJson['Temp']);
+        var speechMsg = "Temperature is " + tempJson['Temp'] + " degree celsius and Humidity is " + tempJson['Hum'] + " percent";
+        var gtts = new gTTS(speechMsg, 'en');
+        gtts.save('outputAudio/say.mp3', function(err, result) {
+            if (err) { throw new Error(err) }
+            playAudio();
+        });
     }
 });
 
